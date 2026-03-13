17c07478465cbf5b7c06109b9ccdf4cf6594e4bf
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2026-03-13 16:32:27 +0600

    chore: fix Relay cask

--- Casks/relay.rb
+++ Casks/relay.rb
@@ -2,7 +2,7 @@ cask "relay" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/hu553in/relay/releases/latest/download/Relay.app.zip"
+  url "https://github.com/hu553in/relay/releases/latest/download/Relay.app.tar.gz"
   name "Relay"
   homepage "https://github.com/hu553in/relay"
 
